312b806ade00cb8cc343253cee005a0bc6f88b09
Aleksey Nikulin <donaldduckisnotdead@gmail.com>
2023-08-11 15:05:57 +0300

    123

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const router = require('./routes');
@@ -17,36 +18,10 @@ mongoose.connect('mongodb://0.0.0.0:27017/mestodb')
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedCors = [
-  'https://alekseynikulin-front15.nomoreparties.co',
-  'http://alekseynikulin-front15.nomoreparties.co',
-  'localhost:3000',
-];
-
-app.use((req, res, next) => {
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-
-    return res.end();
-  }
-});
-
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  next();
-});
-
-app.use(router);
+app.use(cors({
+  origin: 'http://alekseynikulin-front15.nomoreparties.co',
+  credentials: 'include',
+}), router);
 
 app.use((next) => {
   next(new NotFoundError('Страница не найдена'));
