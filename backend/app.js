39479ba6b38aff57667687a48ad2e5518ba61850
Aleksey Nikulin <donaldduckisnotdead@gmail.com>
2023-08-11 15:03:48 +0300

    123

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,20 @@ const allowedCors = [
 ];
 
 app.use((req, res, next) => {
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+
+    return res.end();
+  }
+});
+
+app.use((req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
